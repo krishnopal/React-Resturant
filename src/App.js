2169d2376a3343d02a6114be37df06da033d447b
shuvo pal <spshuvo850@gmail.com>
2021-07-30 09:35:12 +0600

    setup project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Resturant from './component/basice/Resturant';
+
+function App() {
+  return (
+    <div>
+      <Resturant/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/basice/Resturant.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resturant = ()=>{
+    return(
+        <>
+            <div>
+                <h1>Resturant</h1>
+            </div>
+        </>
+    )
+}
+
+export default Resturant;
